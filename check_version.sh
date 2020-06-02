32bfec4d3597ef7c87370bc08b9ef27f5cffdaad
Jon Jagger <jon@jaggersoft.com>
2020-06-02 10:00:04 +0100

    Use full path in check_version.sh

--- check_version.sh
+++ check_version.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -Eeu
-
+readonly MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 readonly REGEX="image_name\": \"(.*)\""
-readonly JSON=`cat docker/image_name.json`
+readonly JSON=`cat ${MY_DIR}/docker/image_name.json`
 [[ ${JSON} =~ ${REGEX} ]]
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
-readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 readonly EXPECTED=2.7.1p83
 readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c 'ruby --version')
 
