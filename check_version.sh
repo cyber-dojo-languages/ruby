d3bf9fb55733299823a6cc35a631967dbad9b9c5
Faye <faye@kosli.com>
2024-11-11 12:20:47 +0100

    Update base image for Ruby 3.3.6 and change check_version

--- check_version.sh
+++ check_version.sh
@@ -5,7 +5,7 @@ readonly JSON=`cat ${MY_DIR}/docker/image_name.json`
 [[ ${JSON} =~ ${REGEX} ]]
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
-readonly EXPECTED="ruby 3.3.5 (2024-09-03 revision ef084cc8f4)"
+readonly EXPECTED="ruby 3.3.6"
 readonly ACTUAL=$(docker run --rm -i ${IMAGE_NAME} sh -c 'ruby --version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
